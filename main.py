3d5487a893b69a1ed50234ea589c407923d877a7
IIYBICT <630061561@qq.com>
2025-09-18 22:09:43 +0800

    init

---
+++ main.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+# @Author  : IIYBICT
+# @Time      :2025/9/18 21:22
+import sys
+import time
+
+from pynput import keyboard
+from pynput.keyboard import Key
+
+
+class KeyboardService:
+    def __init__(self):
+        self.keyboardController = keyboard.Controller()
+
+    def press(self, key):
+        try:
+            special_key = getattr(Key, key)
+            self.keyboardController.press(special_key)
+        except AttributeError:
+            self.keyboardController.press(key)
+
+    def release(self, key):
+        try:
+            special_key = getattr(Key, key)
+            self.keyboardController.release(special_key)
+        except AttributeError:
+            self.keyboardController.release(key)
+
+    def type(self, text):
+        self.keyboardController.type(text)
+
+if __name__ == '__main__':
+    keyboardService = KeyboardService()
+    time.sleep(2)
+
+    if len(sys.argv) > 1:
+        inputType = sys.argv[1]
+        inputKey = sys.argv[2]
+        if inputType == 'press':
+            keyboardService.press(inputKey)
+        elif inputType == 'release':
+            keyboardService.release(inputKey)
+        else:
+            print('Invalid type argument')
+    else:
+        print('Please provide a type argument')
+
+
